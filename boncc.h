d071dc5200fb73590204d2f413ca9db724a00865
KantaTamura <tkanta496@gmail.com>
2022-08-11 12:06:27 +0900

    :sparkles: "return"式を追加

--- boncc.h
+++ boncc.h
@@ -20,6 +20,7 @@ typedef struct Node Node;
 typedef enum {
     TK_IDENT,   // Identifier
     TK_PUNCT,   // Punctuators
+    TK_KEYWORD, // Keywords
     TK_NUM,     // Numeric literals
     TK_EOF,     // End-of-file markers
 } TokenKind;
@@ -73,6 +74,7 @@ typedef enum {
     ND_LT,          // <
     ND_LE,          // <=
     ND_ASSIGN,      // =
+    ND_RETURN,      // "return"
     ND_EXPR_STMT,   // Expression statement
     ND_VAR,         // Variable
     ND_NUM,         // Integer
